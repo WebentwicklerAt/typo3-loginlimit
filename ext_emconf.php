8523366735846619e4593322f66d7ce2d40d5f8e
Dima <dmytro.nozdrin.stratis@gmail.com>
2019-01-04 15:22:27 +0200

    [TASK] Raise version to 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,23 +1,21 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Login limit',
-	'description' => 'Protect backend and/or frontend login against brute-force attacks.',
-	'category' => 'misc',
-	'version' => '1.0.3',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'clearCacheOnLoad' => 1,
-	'author' => 'Gernot Leitgab',
-	'author_company' => 'Webentwickler.at',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-8.7.99',
-			'scheduler' => '6.2.0-8.7.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Login limit',
+    'description' => 'Protect backend and/or frontend login against brute-force attacks.',
+    'category' => 'misc',
+    'version' => '1.1.0',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Gernot Leitgab',
+    'author_company' => 'Webentwickler.at',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-9.5.99',
+            'scheduler' => '8.7.0-9.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
